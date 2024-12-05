c1cdfa9a9bc506c481e5764619624cad57742b31
yeisonmenau <yeisonandresdh@gmail.com>
2024-12-05 11:41:01 -0500

    creacion del proyecto

---
+++ src/main/java/com/cotizador/cotizador_seguros_api/CotizadorSegurosApiApplication.java
@@ -0,0 +1,13 @@
+package com.cotizador.cotizador_seguros_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotizadorSegurosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotizadorSegurosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cotizador/cotizador_seguros_api/CotizadorSegurosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotizador.cotizador_seguros_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotizadorSegurosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
