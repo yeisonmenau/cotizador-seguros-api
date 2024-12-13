12aabc7ff5280cc1ee67bba2dcb4a0444e91f47b
yeisonmenau <yeisonandresdh@gmail.com>
2024-12-12 21:28:39 -0500

    se agrega el CORS

--- src/main/java/com/example/ejemplo_uno/EjemploUnoApplication.java
+++ src/main/java/com/example/ejemplo_uno/EjemploUnoApplication.java
@@ -2,6 +2,9 @@ package com.example.ejemplo_uno;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EjemploUnoApplication {
@@ -9,5 +12,16 @@ public class EjemploUnoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EjemploUnoApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")  // Permite todas las rutas
+						.allowedOrigins("*")  // Permite cualquier origen
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos permitidos
+						.allowedHeaders("*");  // Permite todos los encabezados
+			}
+		};
+	}
 }
